158b05ba5bc52cc81e0aeaa97f8667d22cdb0d82
Emirhan Demirbaşa <emirhanlife@gmail.com>
2026-02-06 01:04:55 +0300

    feature: Giriş arayüzü tasarlandı ve buton işlevleri kodlandı.

---
+++ public/signin.php
@@ -0,0 +1,42 @@
+<?php
+    if (isset($_POST["kayitOl"]) && $_POST["kayitOl"] == "KAYIT OL"){
+        header("Location: signup.php");
+    }
+?>
+
+<style>
+    #login {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        gap: 30px;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        flex-wrap: wrap;
+        width: 100%;
+    }
+</style>
+<?php include "../Views/partials/__header.php" ?>
+<div id="login">
+    <div class="left-div animate__animated animate__fadeInLeft">
+        <h1>WITHBOOKS</h1>
+        <p>Hesabınızı oluşturmak için lütfen kişisel bilgilerini doğru bir şekilde giriniz.</p>
+    </div>
+    <div class="right-div animate__animated animate__fadeInRight">
+        <form method="POST">
+            <div class="form-control">
+                <label for="username">Kullanıcı Adı veya E-mail</label>
+                <input type="text" name="username" placeholder="Kullanıcı adı veya e-mail giriniz...">
+            </div>
+            <div class="form-control">
+                <label for="password">Şifre</label>
+                <input type="password" name="password" placeholder="Şifrenizi girin...">
+            </div>
+            <div class="form-control-button">
+                <button type="submit" name="girisYap" value="GİRİŞ YAP"><i class="fa-solid fa-arrow-right-to-bracket"></i> GİRİŞ YAP</button>
+                <button type="submit" name="kayitOl" value="KAYIT OL"><i class="fa-solid fa-circle-chevron-right"></i> KAYIT OL</button>
+            </div>
+
+        </form>
+    </div>
+</div>
