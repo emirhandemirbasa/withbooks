65f6cf0e9557867d49bfbb2ab57df55f1974010f
Emirhan Demirbaşa <emirhanlife@gmail.com>
2026-02-07 23:29:50 +0300

    feature: Kayıt sınıfı oluşturuldu.
    - passwordControl() ile şifre kontrol fonksiyonu eklendi.
    - emailControl() ile email kontrolü fonksiyonu eklendi.
    - createAccount() ile hesap oluşturma fonksiyonu eklendi.
    - createFakeUserName() ile ilk kayıttaki rastgele kullanıcı adı
      oluşturma algoritması kodladındı ve fonksiyon haline getirildi.
    - isHaveEmail() ile mail kontrol fonksiyonu yazıldı.

---
+++ classes/Register.php
@@ -0,0 +1,85 @@
+<?php
+    class Register{
+        private $email,$password,$repassword,$baglanti;
+
+        function __construct($email,$password,$repassword)
+        {
+            $this->email=$email;
+            $this->password=$password;
+            $this->repassword=$repassword;
+            require $_SERVER["DOCUMENT_ROOT"]."/withbooks/Models/connection.php";
+            $this->baglanti=$baglanti;
+        }
+
+        function getRegAccInfo(){
+            echo "E-mail: ".$this->email."<br>";
+            echo "Şifre: ".$this->password."<br>";
+            echo "Tekrar Şifre: ".$this->repassword."<br>";
+        }
+
+        function passwordControl(){
+            if ($this->password!=$this->repassword){
+                return "ERROR1";
+            }
+            if (!(strlen($this->password)>=8 && strlen($this->password)<=32)){
+                return "ERROR2";
+            }
+            return "SUCCESS";
+        }
+
+        function emailControl(){
+            if (filter_var($this->email,FILTER_VALIDATE_EMAIL) && 
+                preg_match('/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/', $this->email)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        function createAccount(){
+            $fakeUserName = $this->createUserFakeUsername();
+            $sorgu = "INSERT INTO accounts (username,email,password) VALUES (?,?,?)";
+            $hashed_password = password_hash($this->password,PASSWORD_BCRYPT);
+            $stmt = mysqli_prepare($this->baglanti,$sorgu);
+            if (!$stmt){
+                return ["success"=>false,"erorr"=>mysqli_error($this->baglanti)];
+            }
+            if (!mysqli_stmt_bind_param($stmt,"sss",$fakeUserName,$this->email,$hashed_password)){
+                return ["success"=>false,"error"=>mysqli_stmt_error($stmt)];
+            }
+            if (!mysqli_stmt_execute($stmt)){
+                return ["success"=>false,"error"=>mysqli_stmt_error($stmt)];
+            }
+            return ["success"=>true];
+        }
+
+        private function createUserFakeUsername(){
+            $fakeUserName = "";
+            for($i=0;$i<=strlen($this->email);$i++){
+                if ($this->email[$i]!="@"){
+                    $fakeUserName .= $this->email[$i];
+                }else{
+                    break;
+                }
+            }
+            $rastgele = rand(1,100000000);
+            $fakeUserName = $fakeUserName.$rastgele;
+            return $fakeUserName;
+        }
+
+        function isHaveEmail(){
+            $sorgu = "SELECT email FROM accounts WHERE email=?";
+            $stmt = mysqli_prepare($this->baglanti,$sorgu);
+            if (!$stmt){
+                return ["success"=>false,"error"=>mysqli_error($this->baglanti)];
+            }
+            if (!mysqli_stmt_bind_param($stmt,"s",$this->email)){
+                return ["success"=>false,"error"=>mysqli_stmt_error($stmt)];
+            }
+            if (!mysqli_stmt_execute($stmt)){
+                return ["success"=>false,"error"=>mysqli_stmt_error($stmt)];
+            }
+            return ["success"=>true,"isHave"=>(mysqli_num_rows(mysqli_stmt_get_result($stmt))>0)];
+        }
+    }
+?>
